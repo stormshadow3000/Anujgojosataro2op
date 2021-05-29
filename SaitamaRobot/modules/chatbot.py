0bbc8feaa4b21d706fa3fa1d5fa48d019ffdfac7
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-30 03:39:54 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -57,22 +57,261 @@ async def lunaQuery(query: str, user_id: int):
     group=chatbot_group,
 )
 @capture_err
-async def chatbot_talk(_, message):
-    if message.chat.id not in active_chats_bot:
-        return
-    if not message.reply_to_message:
-        return
+async def eren(client, message):
     if message.reply_to_message.from_user.id != BOT_ID:
-        return
-    query = message.text
-    await eren.send_chat_action(message.chat.id, "typing")
-    response = await lunaQuery(
-        query, message.from_user.id if message.from_user else 0
-    )
-    await eren.send_chat_action(message.chat.id, "cancel")
-    await message.reply_text(response)
+        message.continue_propagation()
+    msg = message.text
+    chat_id = message.chat.id
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    if chat_id in en_chats:
+        aura = msg
+        aura = aura.replace("lycia", "Aco")
+        aura = aura.replace("Lycia", "Aco")
+        querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {aura},
+        }
+        headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.text
+        result = result.replace('{"cnt":"', "")
+        result = result.replace('"}', "")
+        result = result.replace("Aco", "Eren")
+        result = result.replace("Eliza", "eren")
+        result = result.replace("Hi~", "Hello Friend I Am Eren Jaeger")
+        result = result.replace("My dear great botmaster, Lyciabot Team.", "Made by Eren Developer team")
+        result = result.replace("Have the control right.", "My Father Is @ihaveeoughhate")
+        result = result.replace("I was created by Lyciabot Team.", "I was created by Eren Developer team.")
+        result = result.replace("<a href=\\", "<a href =")
+        result = result.replace("<\/a>", "</a>")
+        red = result
+        try:
+            await eren.send_chat_action(message.chat.id, "typing")
+            await message.reply_text(red)
+        except CFError as e:
+            print(e)
+    else:
+        u = msg.split()
+        emj = extract_emojis(msg)
+        msg = msg.replace(emj, "")
+        if (
+            [(k) for k in u if k.startswith("@")]
+            and [(k) for k in u if k.startswith("#")]
+            and [(k) for k in u if k.startswith("/")]
+            and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+        ):
 
-__mod__name = "ChatBot"
-__help__ = """
-• /chatbot [ON|OFF] To Enable Or Disable ChatBot In Your Chat.
-"""
+            h = " ".join(filter(lambda x: x[0] != "@", u))
+            km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+            tm = km.split()
+            jm = " ".join(filter(lambda x: x[0] != "#", tm))
+            hm = jm.split()
+            rm = " ".join(filter(lambda x: x[0] != "/", hm))
+        elif [(k) for k in u if k.startswith("@")]:
+
+            rm = " ".join(filter(lambda x: x[0] != "@", u))
+        elif [(k) for k in u if k.startswith("#")]:
+            rm = " ".join(filter(lambda x: x[0] != "#", u))
+        elif [(k) for k in u if k.startswith("/")]:
+            rm = " ".join(filter(lambda x: x[0] != "/", u))
+        elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+            rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+        else:
+            rm = msg
+            lan = translator.detect(rm)
+        aura = rm
+        if not "en" in lan and not lan == "":
+            aura = translator.translate(aura, lang_tgt="en")
+
+        aura = aura.replace("lycia", "Aco")
+        aura = aura.replace("Lycia", "Aco")
+        querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {aura},
+        }
+        headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.text
+        result = result.replace('{"cnt":"', "")
+        result = result.replace('"}', "")
+        result = result.replace("Aco", "eren")
+        result = result.replace("Eliza", "Eren Jaeger")
+        result = result.replace("Hi~", "Hello Friend I Am Eren Jaeger")
+        result = result.replace("My dear great botmaster, Lyciabot Team.", "Made By Eren Developer team")
+        result = result.replace("Have the control right.", "My Father Is Sasuke")
+        result = result.replace("I was created by Lyciabot Team.", "I was created by Eren Developer Team.")
+        result = result.replace("<a href=\\", "<a href =")
+        result = result.replace("<\/a>", "</a>")
+        red = result
+        if not "en" in lan and not lan == "":
+            red = translator.translate(red, lang_tgt=lan[0])
+        try:
+            await eren.send_chat_action(message.chat.id, "typing")
+            await message.reply_text(red)
+        except CFError as e:
+            print(e)
+
+
+
+@eren.on_message(filters.text & filters.private & ~filters.reply & ~filters.bot)
+async def redaura(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        lan = translator.detect(rm)
+    aura = rm
+    if not "en" in lan and not lan == "":
+        aura = translator.translate(aura, lang_tgt="en")
+
+   
+    aura = aura.replace("lycia", "Aco")
+    aura = aura.replace("Lycia", "Aco")
+    querystring = {
+        "bid": "178",
+        "key": "sX5A2PcYZbsN5EY6",
+        "uid": "mashape",
+        "msg": {aura},
+    }
+    headers = {
+        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+        "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = response.text
+    result = result.replace('{"cnt":"', "")
+    result = result.replace('"}', "")
+    result = result.replace("Aco", "Emcee")
+    result = result.replace("Eliza", "@Emcee_Bot")
+    result = result.replace("Hi~", "Hello Friend I Am @Emcee_Bot")
+    result = result.replace("My dear great botmaster, Lyciabot Team.", "Made By Eren Developer team")
+    result = result.replace("Have the control right.", "My Father Is Sasuke Uchiha")
+    result = result.replace("I was created by Lyciabot Team.", "I was created by Eren Developer team")
+    result = result.replace("<a href=\\", "<a href =")
+    result = result.replace("<\/a>", "</a>")
+    red = result
+    if not "en" in lan and not lan == "":
+        red = translator.translate(red, lang_tgt=lan[0])
+    try:
+        await eren.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(red)
+    except CFError as e:
+        print(e)
+
+
+@LYCIA.on_message(
+    filters.regex("eren|EREN|Eren")
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded
+    & ~filters.reply
+    & ~filters.channel
+)
+async def redaura(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        lan = translator.detect(rm)
+    aura = rm
+    if not "en" in lan and not lan == "":
+        aura = translator.translate(aura, lang_tgt="en")
+
+
+    aura = aura.replace("lycia", "Aco")
+    aura = aura.replace("Lycia", "Aco")
+    querystring = {
+        "bid": "178",
+        "key": "sX5A2PcYZbsN5EY6",
+        "uid": "mashape",
+        "msg": {aura},
+    }
+    headers = {
+        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+        "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = response.text
+    result = result.replace('{"cnt":"', "")
+    result = result.replace('"}', "")
+    result = result.replace("Aco", "Emcee")
+    result = result.replace("Eliza", "@Emcee_Bot")
+    result = result.replace("Hi~", "Hello Friend I Am @rikudo_senin_bot")
+    result = result.replace("My dear great botmaster, Daisybot Team.", "Made By Eren Developer team")
+    result = result.replace("Have the control right.", "My Father Is Sasuke Uchiha (@ihaveenoughhate)")
+    result = result.replace("I was created by Lyciabot Team.", "I was created by Eren Developer team.")
+    result = result.replace("<a href=\\", "<a href =")
+    result = result.replace("<\/a>", "</a>")
+    red = result
+    if not "en" in lan and not lan == "":
+        red = translator.translate(red, lang_tgt=lan[0])
+    try:
+        await eren.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(red)
+    except CFError as e:
+        print(e)
+        
+       
