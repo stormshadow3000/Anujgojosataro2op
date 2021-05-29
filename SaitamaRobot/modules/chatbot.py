a768ed48a85181aa09bcb69b899055750889b581
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-29 20:33:46 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -1,4 +1,4 @@
-
+# CREDITS GOES TO @daisyx and Daisyx's Developers
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import re
