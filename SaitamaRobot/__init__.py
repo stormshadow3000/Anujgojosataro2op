7d6ee56d3a0e795b1d99fe022318049417d3d592
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-30 19:16:22 +0530

    Create __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -90,6 +90,7 @@ if ENV:
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
     WALL_API = os.environ.get("WALL_API", None)
     MONGO_URI = os.environ.get("MONGO_DB_URI", None)
+    MONGO_PORT = os.environ.get("MONGO_PORT", None)
     MONGO_DB = os.environ.get("MONGO_DB", None)
     REDIS_URL = os.environ.get("REDIS_URL", None)
     ARQ_API = os.environ.get("ARQ_API", None)
@@ -99,7 +100,7 @@ if ENV:
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
     ARQ_API_URL =  "https://thearq.tech"
     ARQ_API_KEY = ARQ_API
-    MONGO_PORT = get_int_key("MONGO_PORT")
+    
 
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
