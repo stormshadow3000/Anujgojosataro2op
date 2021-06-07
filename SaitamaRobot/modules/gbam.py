8b382d4da0048053daf419e337943e283a648660
stormshadow3000 <83730643+stormshadow3000@users.noreply.github.com>
2021-06-07 15:30:27 +0530

    Update gbam.py

--- SaitamaRobot/modules/gbam.py
+++ SaitamaRobot/modules/gbam.py
@@ -9,10 +9,8 @@ from telegram.ext import CallbackContext, run_async
 
 @run_async
 def gbam(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(random.choice(gbam_strings.GBAM_REASON))
-   
-
-
+    update.effective_message.reply_text(random.choice(gbam_strings.GBAM_REASON))      
+GBAM = "<b>Beginning Of Global Bam For {user2}</b>  \n \nChat Id : <code>{chatid}</code> \nReason : <i>{reason}</i> \nGBammed By {user1}"
 GBAM_REASON_HANDLER = DisableAbleCommandHandler("gbam", gbam)
 
 dispatcher.add_handler(GBAM_REASON_HANDLER)
